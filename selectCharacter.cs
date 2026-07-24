d604f2b72f3328b1ef95bf4feea2dd22b6f372e0
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2026-07-25 00:42:02 +0100

    Add selectCharacter class with character selection

---
+++ selectCharacter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectCharacter : MonoBehaviour {
+
+  [SerializeField]
+  CameraFollow cameraFollow;
+
+  public Character[] characters;
+  public int selectedCharacter = 0;
+
+  
+  
+}
+
